544839541ae8106019b77039854d7cdb0ba5ae4c
Jeriah Bowers <jeriah97bowers@yahoo.com>
2017-09-17 21:39:05 -0400

    final changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
+
+
 $("#alert img").on("click", function() {
     $("#alert").remove();
     $("#noti").remove();
     $("#bell").css("margin-right", "1em");
 });
 
+$("#subme").on("click", function() {
+    if ($("textarea").val() == "") {
+        alert("Sorry, must enter a message!");
+    } else {
+        $("#subme").html("Success!");
+        $("#subme").css("background-color", "lime");
+        $("textarea").val("");
+        $("textarea").attr("placeholder", "Message for User"); 
+    }
+});
